fffc28c0330fceafa580c8d8324acee51aa0af2b
gyurza921 <objekt.172.uru@gmail.com>
2020-07-29 01:00:20 +0900

    main.js を変更

--- main.js
+++ main.js
@@ -5,6 +5,38 @@
 // 配列をクイックソートする関数
 // 
 
-function quickSort(value){
+function quickSort(array, start, end){
+
+    // startとendが逆転していたら終わり
+    if(end <= start){
+        return;
+    }
+
+    // ピボットは先頭の物にする
+    // のちの改良ではここをもう少し工夫する
+    const pivot = array[start];
+
+    // 配列の前半と後半に振り分ける
+    let left = start;
+    let right = end;
+    // このままでは無限ループになる
+    while(true){
+
+        while(array[left] < pivot) { left++ } // pivot以上の値を左から探す
+        while(array[right] > pivot) { right-- } // pivot以下の値を右から探す
+        if(left >= right) { break; } // leftとrightが逆転したらwhileを抜ける
+        [array[left], array[right]] = [array[right], array[left]]; // 値の入れ替え
+        left++;
+        right--;
+
+    }
+
+    // 配列の前半と後半をそれぞれクイックソート
+    quickSort(array, start, left - 1);
+    quickSort(array, right + 1, end);
     
 }
+
+const array = [10, 8, 4, 3, 7, 1, 5, 6, 2, 9, ];
+quickSort(array, 0, array.length - 1);
+console.log(array);
