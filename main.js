cff87ed3de0bc2c20b627b0f7bdeaee2c0ad508b
gyurza921 <objekt.172.uru@gmail.com>
2020-07-29 01:21:19 +0900

    作成途中

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ function quickSort(array, start, end){
     // 配列の前半と後半に振り分ける
     let left = start;
     let right = end;
+
     // このままでは無限ループになる
     while(true){
 
@@ -37,6 +38,42 @@ function quickSort(array, start, end){
     
 }
 
-const array = [10, 8, 4, 3, 7, 1, 5, 6, 2, 9, ];
+
+// 
+// 降順にソートする関数関数
+// 
+
+function quickSortDecend(array, start , end) {
+
+    // start と endが逆転したら終わり
+    if(end <= start) {
+        return;
+    }
+
+    // ピボットは先頭の物にする
+    // のちの改良ではここをもう少し工夫する
+    const pivot = array[start];
+
+    // 配列の前半と後半に振り分ける
+    let left = start;
+    let right = end;
+
+    // このままでは無限ループになる
+    while(true){
+
+        while(array[left] < pivot) { left++ } // pivot以上の値を左から探す
+        while(array[right] > pivot) { right-- } // pivot以下の値を右から探す
+        if(left >= right) { break; } // leftとrightが逆転したらwhileを抜ける
+        [array[left], array[right]] = [array[right], array[left]]; // 値の入れ替え
+        left++;
+        right--;
+
+    }
+
+    // 配列の前半と後半をそれぞれ降順にクイックソート
+
+}
+
+const array = [10, 8, 4, 3, 7, 1, 5, 6, 2, 9];
 quickSort(array, 0, array.length - 1);
 console.log(array);
