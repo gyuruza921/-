065561f45a83634643a2e4641bffb6f247ec6181
gyurza921 <objekt.172.uru@gmail.com>
2020-07-30 00:18:48 +0900

    add function generatenumber

--- main.js
+++ main.js
@@ -2,47 +2,20 @@
 "use strict"
 
 // 
-// 配列をクイックソートする関数
+// ドキュメント部から各要素を取得
 // 
 
-function quickSort(array, start, end){
+const btnStart = document.getElementById("start");
 
-    // startとendが逆転していたら終わり
-    if(end <= start){
-        return;
-    }
+const disp1 = document.getElementById("dips1");
 
-    // ピボットは先頭の物にする
-    // のちの改良ではここをもう少し工夫する
-    const pivot = array[start];
-
-    // 配列の前半と後半に振り分ける
-    let left = start;
-    let right = end;
-
-    // このままでは無限ループになる
-    while(true){
-
-        while(array[left] < pivot) { left++ } // pivot以上の値を左から探す
-        while(array[right] > pivot) { right-- } // pivot以下の値を右から探す
-        if(left >= right) { break; } // leftとrightが逆転したらwhileを抜ける
-        [array[left], array[right]] = [array[right], array[left]]; // 値の入れ替え
-        left++;
-        right--;
-
-    }
-
-    // 配列の前半と後半をそれぞれクイックソート
-    quickSort(array, start, left - 1);
-    quickSort(array, right + 1, end);
-    
-}
-
+const disp2 = document.getElementById("disp2");
 
 // 
-// 降順にソートする関数関数
+// 処理部
 // 
 
+// 降順にソートする関数
 function quickSortDecend(array, start , end) {
 
     // start と endが逆転したら終わり
@@ -79,11 +52,38 @@ function quickSortDecend(array, start , end) {
 
 const array = [10, 8, 4, 3, 7, 1, 5, 6, 2, 9];
 
-console.log(array);
-
-quickSort(array, 0, array.length - 1);
-console.log(array);
-
 // 降順にソート
 quickSortDecend(array, 0, array.length - 1);
 console.log(array);
+
+// 
+// 操作部
+// 
+
+// 値のランダムな配列を作る
+
+// 空の配列を作る
+const array1 = []
+
+// 配列にランダムな値を加える関数
+function addRandomValue(array, n) {
+
+    // 
+    for(let i = 0; i < n; i++){
+
+        const min = 0;
+
+        const max = Math.floor(n);
+
+        const value = Math.floor( Math.random() * (max - min)) + min ;
+
+        console.log(value);
+
+        array.push(value);
+    }
+    
+}
+
+addRandomValue(array1, 100);
+
+console.log(array1);
